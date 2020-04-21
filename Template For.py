3a86788731c1c3d5c75f559ce8c1fd08419ddf15
illulia <choidaehoon0507@gmail.com>
2020-04-21 22:13:06 +0900

    20200421

---
+++ Template For.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def helloworld():
+    return "Hello Flask World!"
+
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+app.run()
