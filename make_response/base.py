7d9ef8ff2b28b4daf5c27dbe084418578fad6dce
illulia <choidaehoon0507@gmail.com>
2020-04-21 19:06:54 +0900

    20200421

--- make_response/base.py
+++ make_response/base.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 app.debug = True 
 
 @app.route('/res1')
-def res1():                               # status # 헤더에 은밀한값 보내기
+def res1():                              
     custom_res = Response("Custom Response", 200, {'test': 'ttt'})
     return make_response(custom_res)
-          #큰 파일이나 데이터 보낼때 더 가벼움 (밑에 코드 보다)
+
 @app.route("/gg")
 def helloworld2():
     return "Hello Flask World!" + getattr(g, 'str', '111')
